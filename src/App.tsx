5bb1c551bc25f7871e8eaf97c5ae88b31afe5331
yschoi <yschoi9733@gmail.com>
2025-05-11 00:02:48 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+      <img
+        className="size-12 shrink-0"
+        src="/img/logo.svg"
+        alt="ChitChat Logo"
+      />
+      <div>
+        <div className="text-xl font-medium text-black dark:text-white">
+          ChitChat
+        </div>
+        <p className="text-gray-500 dark:text-gray-400">
+          You have a new message!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+});
