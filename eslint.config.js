05b3081575439fc34bde6f65b661fa8b0f59cb66
yschoi <yschoi9733@gmail.com>
2025-05-17 00:41:16 +0900

    style: rules error -> warn

--- eslint.config.js
+++ eslint.config.js
@@ -36,13 +36,14 @@ export default tseslint.config(
     rules: {
       ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': 'warn',
       'react/react-in-jsx-scope': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
       'prettier/prettier': [
-        'error',
+        'warn',
         {
           endOfLine: 'auto',
         },
