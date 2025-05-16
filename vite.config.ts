4b654a8affc7367ab51288662606dd2d42945642
yschoi <yschoi9733@gmail.com>
2025-05-17 00:41:54 +0900

    feat: add vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
+  plugins: [react(), react(), tsconfigPaths(), tailwindcss()],
 });
